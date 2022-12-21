66dd249135cfc025f778d0dc39cddf8ea5350ec2
Kurosaki Aki <kurosakiaki890@gmail.com>
2022-12-21 23:23:54 +0700

    deploy

--- src/utils/httpRequest.js
+++ src/utils/httpRequest.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpRequest = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL,
+    baseURL: "https://trackscan-be.azurewebsites.net/",
 });
 
 export const get = async (path, options = {}) => {
