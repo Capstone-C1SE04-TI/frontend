ae2682354e01a9dc75946a2d03e7dd51b19e7c09
Kurosaki Aki <kurosakiaki890@gmail.com>
2022-12-21 23:26:49 +0700

    deploy

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -4,7 +4,7 @@ const images = {
     facebook: require('./facebook.svg').default,
     telegram: require('./telegram.svg').default,
     twitter: require('./twitter.svg').default,
-    banner: require('./banner.svg').default,
+    // banner: require('./banner.svg').default,
     noImage: require('./no-image.png'),
     etherium: require('./etherium.svg').default,
     logoRobot: require('./logo-robot.png'),
